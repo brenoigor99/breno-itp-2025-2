9e4a60062cf2428c7382edb25153ad38418f6769
brenoigor99 <brenoigor_100@hotmail.com>
2025-10-22 22:40:55 -0300

    Upload do projeto e dos exercícios das semanas de 2 a 8

---
+++ listas/semana8-repeticoes-aninhadas/problema1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int m;
+    int n;
+    int sorteados[30], apostados[50];
+    int acertos = 0;
+
+    scanf("%d %d", &m, &n);
+    /*Lê os m números sorteados*/
+    for(int i = 0; i < m; i++){
+        scanf("%d", &sorteados[i]);
+    }
+    /*Lê os n números apostados*/
+    for(int i = 0; i < n; i++){
+        scanf("%d", &apostados[i]);
+    }
+    /*Para cada número apostado, verifica se está entre os sorteados*/
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(apostados[i] == sorteados[j]){
+                acertos++;
+                break;
+            }
+        }
+    }
+    printf("%d\n", acertos);
+    return 0;
+}
