45e6c778ba942aab226418604decca3a6587a4c4
brenoigor99 <brenoigor_100@hotmail.com>
2025-11-09 23:26:06 -0300

    Upload das listas 2 a 10

--- listas/semana10-ponteiros-alocacao/problema1.c
+++ listas/semana10-ponteiros-alocacao/problema1.c
@@ -1,70 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* alocarVetor(int n){
-    int* vetor;
-    vetor = (int*) malloc(n * sizeof(int));
-    return vetor;
-}
-
-int* somaVetores(int* u, int* v, int n){
-    int* resultado;
-    int i;
+int *somaVetores(int *a, int n1, int *b, int n2){
+    int *res;
     
-    resultado = alocarVetor(n);
+    res = malloc(n1 * sizeof(int));
     
-    for(i = 0; i < n; i++) {
-        resultado[i] = u[i] + v[i];
+    for(int i = 0; i < n1; i++){
+        res[i] = a[i] + b[i];
     }
-    
-    return resultado;
+    return res;
 }
 
 int main()
 {
-    int n1, n2;
-    int* u;
-    int* v;
-    int* soma;
-    int i;
+    int *u, *v, *vetor_soma, n1, n2, i;
     
     scanf("%d %d", &n1, &n2);
     
-    // Aloca os vetores
-    u = alocarVetor(n1);
-    v = alocarVetor(n2);
-    
-    // Lê os valores de u
-    for(i = 0; i < n1; i++){
-        scanf("%d", &u[i]);
-    }
-
-    // Lê os valores de v
-    for(i = 0; i < n2; i++){
-        scanf("%d", &v[i]);
-    }
-
-    // Verifica se as dimensões são compatíveis
     if(n1 != n2){
         printf("dimensoes incompativeis\n");
     }
     else{
-        soma = somaVetores(u, v, n1);
-        // Imprime o resultado
-        for(i = 0; i < n1; i++) {
-            printf("%d", soma[i]);
-            if(i < n1 - 1) {
-                printf(" ");
-            }
+        // Alloca dinamicamente os vetores
+        u = malloc(n1 * sizeof(int));
+        v = malloc(n2 * sizeof(int));
+        
+        // Lê o vetor u
+        for(i = 0; i < (n1 - 1); i++){
+            scanf("%d", &u[i]);
+        }
+        scanf("%d", &u[n1-1]);
+        
+        // Lê o vetor v
+        for(i = 0; i < (n2 - 1); i++){
+            scanf("%d", &v[i]);
+        }
+        scanf("%d", &v[n2-1]);
+        
+        vetor_soma = somaVetores(u, n1, v, n2);
+        
+        // Imprime o vetor resultante da soma
+        for(i = 0; i < n1; i++){
+            printf("%d ", vetor_soma[i]);
         }
         printf("\n");
-        
-        free(soma);
     }
-
-    // Libera a memória
-    free(u);
-    free(v);
     
     return 0;
 }
